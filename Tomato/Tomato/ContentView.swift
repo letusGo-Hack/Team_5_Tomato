3a038286d530267b689e9863545f8cafca63d4b2
Glsme <glasses.str.man@gmail.com>
2024-06-29 11:26:16 +0900

    Feat: create Project

---
+++ Tomato/Tomato/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tomato
+//
+//  Created by Seokjune Hong on 6/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tomato/Tomato/TomatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoApp.swift
+//  Tomato
+//
+//  Created by Seokjune Hong on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TomatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
