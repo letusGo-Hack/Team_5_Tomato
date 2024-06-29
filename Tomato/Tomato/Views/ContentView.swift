339b1ecdb715fde9de42284e66e953ba4067a94c
happyyyyyyyyyy <hsmikvyv@gmail.com>
2024-06-29 13:12:29 +0900

    feat($main): Main에 Timer 추가

---
+++ Tomato/Tomato/Views/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Tomato
+//
+//  Created by Seokjune Hong on 6/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack(spacing: 30) {
+      Text("Tomato Timer")
+        .font(.title)
+        .foregroundStyle(.pink)
+      
+      CustomTimer()
+      
+      Spacer()
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
